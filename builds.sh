4345c7728621c6aeae22afce79e7030aac685f30
Chris Sibbitt <csibbitt@gmail.com>
2024-05-30 22:21:57 -0400

    Add android to builds.sh

--- builds.sh
+++ builds.sh
@@ -14,5 +14,20 @@ mkdir _build_win32
 cd _build_win32
 cmake .. -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw.toolchain -DSERIOUS_PROTON_DIR=../../SeriousProton -DCMAKE_PROJECT_EmptyEpsilon_INCLUDE=./version.cmake
 ninja
+ninja package
+
+cd ..
+rm -Rf _build_android
+cd _build_android
+cmake .. -G Ninja -DSERIOUS_PROTON_DIR=../../SeriousProton -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain -DCMAKE_PROJECT_EmptyEpsilon_INCLUDE=./version.cmake
+ninja
+
+cd ..
+
+rm -Rf _build_android_64
+mkdir _build_android_64
+cd _build_android_64
+cmake .. -G Ninja -DSERIOUS_PROTON_DIR=../../SeriousProton -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain -DANDROID_ABI=arm64-v8a  -DCMAKE_PROJECT_EmptyEpsilon_INCLUDE=./version.cmake
+ninja
 
 cd ..
