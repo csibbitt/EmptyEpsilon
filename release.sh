3d4268b12c968736c0938de68a8b54290f891848
Chris Sibbitt <csibbitt@gmail.com>
2025-03-18 13:01:53 -0400

    fix version and release script

--- release.sh
+++ release.sh
@@ -6,10 +6,12 @@ MINOR=$(grep MINOR version.cmake | awk '{print $2}' | sed -e 's/)//')
 PATCH=$(grep PATCH version.cmake | awk '{print $2}' | sed -e 's/)//')
 TITLETAG="${MAJOR}${MINOR}${PATCH}"
 
-echo "Forked build w/ 10,000 entry ship's log. This is the ${ORIG_TAG} release with the following additions:
+echo "Forked build w/ support for very large maps. This is the ${ORIG_TAG} release with the following additions:
 
-daid/SeriousProton#239
-daid#1938" |
+* Larger zoom limits on Relay, Spectator, and GM screens (https://github.com/csibbitt/EmptyEpsilon/commit/6bfbc37)
+* Relay Button to center on ship (daid#2298 - https://github.com/csibbitt/EmptyEpsilon/commit/f3484d3)
+* Three-letter sector names for huge maps (https://github.com/csibbitt/EmptyEpsilon/commit/ad259acd)
+" |
 gh release create -d -R csibbitt/EmptyEpsilon -F - -t "${TITLETAG}" "${CSBUILD_TAG}" \
 '_build_win64/EmptyEpsilon.zip#Windows ZIP' \
 '_build_android/EmptyEpsilon-armeabi-v7a.apk#Android arm APK' \
